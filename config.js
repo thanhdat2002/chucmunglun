4f0522ffb3d6c9bf11ba0aca2394867400360022
thanhdat2002 <80498296+thanhdat2002@users.noreply.github.com>
2022-12-25 22:08:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Lùn';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Merry christmars';
 const titleCard = 'Dear đến Con Lùn ';
-const contentCard = 'Gữi đến con Lanh vừa lùn vừa khó chịu tuy nhiên lại rất chi là kĩ tính và gọn gang. Cảm ơn cuộc gặp gỡ thật sự như l của chúng ta. Tuy Nhiên mong vẫn là Tình bạn đẹp . Người Gữi " Thằng Sài Gòn"';
+const contentCard = 'Gữi đến con Lanh vừa lùn vừa khó chịu tuy nhiên lại rất chi là kĩ tính và gọn gang. Cảm ơn cuộc gặp gỡ thật sự như l của chúng ta. Tuy Nhiên mong vẫn là Tình bạn đẹp MERRY CHRISTMAS. Người Gữi " Thằng Sài Gòn"';
 
 
 const giftImage = 'Lanh.png';
