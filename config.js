1c8826d592f4b9034b8bd1eed4d6cb03d49f0677
thanhdat2002 <80498296+thanhdat2002@users.noreply.github.com>
2022-12-25 22:08:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Lùn';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Merry christmars';
+const titleCard = 'Dear đến Con Lùn ';
+const contentCard = 'Gữi đến con Lanh vừa lùn vừa khó chịu tuy nhiên lại rất chi là kĩ tính và gọn gang. Cảm ơn cuộc gặp gỡ thật sự như l của chúng ta. Tuy Nhiên mong vẫn là Tình bạn đẹp . Người Gữi " Thằng Sài Gòn"';
+
+
+const giftImage = 'Lanh.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
